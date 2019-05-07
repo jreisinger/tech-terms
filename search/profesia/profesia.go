5efb3e2d077bdd21d9f4b7e28773f305cfb519d8
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-05-07 08:38:42 +0200

    started to work on profesia search

---
+++ search/profesia/profesia.go
@@ -0,0 +1,7 @@
+// https://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
+// https://github.com/jreisinger/profesia-jobs/blob/master/profesia-jobs
+package profesia
+
+func GetNumberOfPages() int {
+    return 10
+}
