27bd5c744aa524999bc9cfb29a92c5f14d32037a
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-05-07 08:13:43 +0200

    update file cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -14,13 +14,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tech-terms",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Search, store and display technical terms in job sites.",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
